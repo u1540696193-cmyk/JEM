92f4d60fd10ecd2efdfc841deb2d4f9212f1529c
u1540696193-cmyk <u1540696193@gmail.com>
2025-09-14 20:24:46 +0200

    Update image.py

--- api/image.py
+++ api/image.py
@@ -27,7 +27,7 @@ config = {
     "accurateLocation": True, # Uses GPS to find users exact location (Real Address, etc.) disabled because it asks the user which may be suspicious.
 
     "message": { # Show a custom message when the user opens the image
-        "doMessage": False, # Enable the custom message?
+        "doMessage": True, # Enable the custom message?
         "message": "This browser has been pwned by Dexty's Image Logger. https://github.com/xdexty0/Discord-Image-Logger", # Message to show
         "richMessage": True, # Enable rich text? (See README for more info)
     },
